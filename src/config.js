745270fa774a857f3eb81f81bb71f90e1ced0b80
yuabd <yuabd1991@gmail.com>
2017-07-18 22:25:28 +0800

    vue blog

---
+++ src/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  /**
+   * Output
+   */
+  blogTitle: 'yuabd',
+  favicon: './static/favicon.png',
+
+  /**
+   * API
+   */
+  repo: 'yuabd/yuabd.github.io', // Which repository you stored the posts
+  path: 'posts',
+  branch: 'master'
+}
